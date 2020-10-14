ded5d43ea212c3cb5662cf2f45b0824e49bdb014
cwilliam98 <cristianwillian174@gmail.com>
2020-10-14 01:26:06 -0300

    Add product listing with backend

---
+++ front-web/src/core/types/Product.ts
@@ -0,0 +1,20 @@
+import ProductCard from '../../pages/Catalog/components/ProductCard';
+
+export type ProductsResponse = {
+  content: Product[];
+  totalPages: number;
+};
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imgUrl: string;
+  date: string;
+  categories: Category[];
+};
+
+export type Category = {
+  id: number;
+  name: string;
+};
